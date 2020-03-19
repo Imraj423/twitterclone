1705d01286843789e135a551f9748cf64f525ff6
Imraj423 <48530428+Imraj423@users.noreply.github.com>
2020-03-19 09:23:49 -0400

    change tweet char length

--- tweet/models.py
+++ tweet/models.py
@@ -4,7 +4,7 @@ from twitteruser.models import TwitterUser
 
 
 class Tweet(models.Model):
-    body = models.CharField(max_length=280)
+    body = models.CharField(max_length=140)
     time = models.DateTimeField(default=timezone.now)
     twitter_user = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
 
